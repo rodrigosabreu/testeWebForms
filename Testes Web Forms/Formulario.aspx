61b3540cde9cdab0801af3b2a6d3333c61be37da
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2021-04-25 15:19:10 -0300

    Testes com Web Forms

---
+++ Testes Web Forms/Formulario.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Formulario" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Formulario.aspx.cs" Inherits="Formulario" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h2><%: Title %>.</h2>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+        <Columns>
+            <asp:BoundField DataField="nome" HeaderText="Nome" SortExpression="nome" />
+            <asp:BoundField DataField="email" HeaderText="E-mail" SortExpression="email" />
+        </Columns>
+    </asp:GridView>
+
+
+    <br />
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+    <br />
+    <br />
+    <br />
+    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+    </asp:DropDownList>
+
+
+</asp:Content>
+
---
+++ Testes Web Forms/Formulario.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+class Pessoa
+{
+    public string id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
+
+
+
+
+public partial class Formulario : System.Web.UI.Page
+{
+
+    private List<Pessoa> listaPessoa;
+    string sessao = "dfasdfs";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {   
+  
+        if (Session[sessao] == null)
+        {
+            listaPessoa = new List<Pessoa>();
+        }
+        else
+        {            
+            listaPessoa = (List<Pessoa>)Session[sessao];
+            CarregaGV();
+        }
+    }
+
+
+    private void CarregaGV()
+    {        
+        GridView1.DataSource = listaPessoa.OrderBy(x => x.Nome).ToList();     
+        GridView1.DataBind();   
+        Label1.Text = "O total é: " + listaPessoa.Count;
+
+        CarregaDrop();
+
+    }
+
+
+    private void CarregaDrop()
+    {
+        List<string> list = new List<string>();
+
+
+        DropDownList1.DataValueField = "id";
+        DropDownList1.DataTextField = "Nome";
+        
+        /*foreach (var item in listaPessoa)
+        {
+            list.Add(item.Nome + " - " + item.Email);
+        }*/
+
+        DropDownList1.DataSource = listaPessoa;
+        DropDownList1.DataBind();
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        var p = new Pessoa();
+        p.Nome = TextBox1.Text;
+        p.Email = TextBox2.Text;
+               
+        if (Session[sessao] == null)        
+            listaPessoa = new List<Pessoa>();          
+        else        
+            listaPessoa = (List<Pessoa>)Session[sessao];
+
+        listaPessoa.Add(p);        
+        Session[sessao] = listaPessoa;
+        CarregaGV();
+    }
+
+
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
+
